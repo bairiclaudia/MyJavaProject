ebeb260195bf7ca44c13cb4ecbcbcd4be3231a6a
bairiclaudia <151497596+bairiclaudia@users.noreply.github.com>
2023-11-21 12:14:34 +0530

    Update Welcome.java

--- src/com/edubridge/app2/Welcome.java
+++ src/com/edubridge/app2/Welcome.java
@@ -3,5 +3,6 @@ package com.edubridge.app2;
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("welcome to GitHub");
+		System.out.println("Welcome to Java");
 	}
 }
